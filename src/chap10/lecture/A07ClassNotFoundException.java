2430e07ff019ebe6a699c6cf08f59a1f40f995f2
Hesitater <feelksh0603@gmail.com>
2021-10-06 19:44:39 +0900

    Update A07ClassNotFoundException.java

--- src/chap10/lecture/A07ClassNotFoundException.java
+++ src/chap10/lecture/A07ClassNotFoundException.java
@@ -1,6 +1,6 @@
 package chap10.lecture;
 
-public class A7ClassNotFoundException {
+public class A07ClassNotFoundException {
 	public static void main(String[] args) {
 		try {
 			Class.forName("p10.lecture.A7ClassNotFoundException");

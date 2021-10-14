a98f27f5184f7964cfa8385cefc86f73b53db465
Hesitater <feelksh0603@gmail.com>
2021-10-14 18:58:58 +0900

    equals

--- src/chap11/example/sec110301/MemberExample.java
+++ src/chap11/example/sec110301/MemberExample.java
@@ -5,18 +5,23 @@ public class MemberExample {
 		Member obj1 = new Member("blue");
 		Member obj2 = new Member("blue");
 		Member obj3 = new Member("red");
-
-		if (obj1.equals(obj2)) { 		// 매개값이 Member 타입이고 id 필드값도 동일하므로 true
-			System.out.println("obj1 과 obj2는 동등합니다");
+		
+		
+		System.out.println(obj1.equals(obj1));
+		System.out.println(obj1.equals(obj2));
+		
+		
+		if(obj1.equals(obj2)) {
+			System.out.println("obj1과 obj2는 동등합니다");
 		} else {
-			System.out.println("obj1 과 obj2는 동등하지 않습니다");
+			System.out.println("obj1과 obj2는 동등하지 않습니다.");
 		}
-
-		if (obj1.equals(obj3)) {		 // 매개값이 Member 타입이지만 id 필드값이 다르므로 false
-			System.out.println("obj1과 obj3은 동등합니다");
+		
+		if(obj1.equals(obj3)) {
+			System.out.println("obj1과 obj3은 동등합니다.");
 		} else {
-			System.out.println("obj1과 obj3은 동등하지 않습니다");
+			System.out.println("obj1과 obj3은 동등하지 않습니다.");
 		}
-
+		
 	}
 }

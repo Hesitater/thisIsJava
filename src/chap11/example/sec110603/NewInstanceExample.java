4f12fac3a1a799afb641934ef4bf2b57026285cb
Hesitater <feelksh0603@gmail.com>
2021-11-03 19:07:44 +0900

    Dynamic object creation.

--- src/chap11/example/sec110603/NewInstanceExample.java
+++ src/chap11/example/sec110603/NewInstanceExample.java
@@ -3,8 +3,8 @@ package chap11.example.sec110603;
 public class NewInstanceExample {
 	public static void main(String[] args) {
 		try {
-//			Class clazz = Class.forName("chap11.example.sec110603.SendAction");
-			Class clazz = Class.forName("chap11.example.sec110603.ReceiveAction");
+			Class clazz = Class.forName("chap11.example.sec110603.SendAction");
+//			Class clazz = Class.forName("chap11.example.sec110603.ReceiveAction");
 			Action action = (Action) clazz.newInstance();
 			action.execute();
 		} catch (ClassNotFoundException e) {

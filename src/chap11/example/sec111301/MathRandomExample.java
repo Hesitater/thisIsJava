64c009ca10121f94549828d835d9939e5778d48e
Hesitater <feelksh0603@gmail.com>
2021-11-27 13:22:29 +0900

    dice 1~6

---
+++ src/chap11/example/sec111301/MathRandomExample.java
@@ -0,0 +1,12 @@
+package chap11.example.sec111301;
+
+public class MathRandomExample {
+	public static void main(String[] args) {
+		
+		int num = (int) (Math.random()*6) +1;
+		System.out.println("주사위 눈:" + num);
+		
+		int numlo1 = (int) (Math.random()*45) + 1;
+		System.out.println("로또번호중하나:" + numlo1);
+	}
+}
